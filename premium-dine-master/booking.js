3ba8db15e3d8b24dd813d2e62c319eeed037a4a7
Zuoli Zhu <zuolizhu5960x@gmail.com>
2020-04-26 19:47:44 -0400

    form validation updated

--- premium-dine-master/booking.js
+++ premium-dine-master/booking.js
@@ -8,6 +8,19 @@ const dropdownArrow = document.getElementById('dropdown-arrow');
 const numberOfPeople = document.getElementById('number-of-people');
 const minusButton = document.getElementById('minus');
 const plusButton = document.getElementById('plus');
+//vars for reservationButton
+const reservationButton = document.getElementById('btn-reservation');
+//vars for form elements
+const name = document.querySelector("input[name='name']");
+const email = document.querySelector("input[name='email']");
+const month = document.querySelector("input[name='month']");
+const day = document.querySelector("input[name='day']");
+const year = document.querySelector("input[name='year']");
+const hour = document.querySelector("input[name='hour']");
+const minute = document.querySelector("input[name='minute']");
+const errors = document.querySelectorAll('p.errormsg');
+const inputboxes = document.querySelectorAll('input.input-box');
+const datetimeFields = document.querySelectorAll('p.datetime-field-name');
 
 dropdownTrigger.onclick = () => {
   dropdownMenu.classList.toggle('open');
@@ -39,3 +52,59 @@ function changeNumberOfPeople(isPlus) {
     }
   }
 }
+
+reservationButton.onclick = () => {
+  formValidate();
+}
+
+function formValidate() {
+  if (name.value == '') {
+    errors[0].classList.add('show');
+    inputboxes[0].classList.add('error');
+  }
+  if (email.value == '') {
+    errors[1].classList.add('show');
+    inputboxes[1].classList.add('error');
+  }
+  if (month.value == '' || day.value == '' || year.value == '') {
+    errors[2].classList.add('show');
+    datetimeFields[0].classList.add('error');
+    if (month.value == '') {
+      inputboxes[2].classList.add('error');
+    }
+    if (day.value == '') {
+      inputboxes[3].classList.add('error');
+    }
+    if (year.value == '') {
+      inputboxes[4].classList.add('error');
+    }
+  }
+  if (hour.value == '' || minute.value == '') {
+    errors[3].classList.add('show');
+    datetimeFields[1].classList.add('error');
+    if (hour.value == '') {
+      inputboxes[5].classList.add('error');
+    }
+    if (minute.value == '') {
+      inputboxes[6].classList.add('error');
+    }
+  }
+}
+
+inputboxes.forEach((box, i) => {
+  // remove error status when focus
+  box.onfocus = () => {
+    box.classList.remove('error');
+    if (i == 0) {
+      errors[0].classList.remove('show');
+    } else if (i == 1) {
+      errors[1].classList.remove('show');
+    } else if (i > 1 && i < 5) {
+      errors[2].classList.remove('show');
+      datetimeFields[0].classList.remove('error');
+    } else {
+      errors[3].classList.remove('show');
+      datetimeFields[1].classList.remove('error');
+    }
+  }
+});
