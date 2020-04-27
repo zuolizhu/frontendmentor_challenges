a448e8b81ace1bb9a12373848bf6be0689f43925
Zuoli Zhu <zuolizhu5960x@gmail.com>
2020-04-27 01:31:38 -0400

    fix url, breakpoint bug

--- premium-dine-master/slide.js
+++ premium-dine-master/slide.js
@@ -4,7 +4,7 @@ const slideContent = document.getElementById('slide-content');
 const events = document.querySelectorAll('li.gathering__event');
 const underlines = document.querySelectorAll('div.gathering__event__underline');
 
-const breakPoints = [768, 1200];
+const breakPoints = [767, 1200];
 const windowSize = window.innerWidth;
 
 const slides = [
@@ -27,12 +27,14 @@ const slides = [
 const imageUrlSuffixes = ['mobile@2x.jpg', 'tablet@2x.jpg', 'desktop@2x.jpg'];
 
 let suffix = 0;
+console.log('current size: ', windowSize);
 if (windowSize > breakPoints[0] && windowSize < breakPoints[1]) {
   suffix = 1;
 }
 else if (windowSize > breakPoints[1]) {
   suffix = 2;
 }
+console.log('suffix: ', suffix);
 
 slideImage.innerHTML = "";
 slideImage.appendChild(generateImageComponent(0, suffix));
