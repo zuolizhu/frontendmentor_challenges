1b7e168dd8e83df2b01366700b327e9b573a9b66
zuolizhu <zuolizhu5960x@gmail.com>
2020-06-11 18:45:47 -0400

    fix validation not load issue

--- premium-minimalist-portfolio-master/app.js
+++ premium-minimalist-portfolio-master/app.js
@@ -39,3 +39,38 @@ burgerButton.onclick = () => {
   burgerIcon.classList.toggle('hide');
   closeIcon.classList.toggle('hide');
 }
+
+init();
+
+swup.on('contentReplaced', init);
+
+// form validation
+function init() {
+  if (window.location.pathname.includes('contact')) {
+    const inputFields = document.querySelectorAll('.contact-form__input-field');
+    const contactForm = document.getElementById('contact-form');
+    
+    // validation on submit
+    contactForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+    
+      inputFields.forEach(inputField => {
+        if (inputField.value.length == 0) {
+          // input form alert border
+          inputField.classList.add('error');
+          inputField.nextElementSibling.classList.remove('hide');
+        }
+      });
+    
+      return false;
+    });
+    
+    // form on focus
+    inputFields.forEach(inputField => {
+      inputField.addEventListener('focus', () => {
+        inputField.classList.remove('error');
+        inputField.nextElementSibling.classList.add('hide');
+      });
+    });
+  }
+}
