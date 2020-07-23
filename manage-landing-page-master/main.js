81dc50fa4b034617b0e4e471c738f5a47e0d94d4
zuolizhu <zuolizhu5960x@gmail.com>
2020-07-23 17:25:37 -0400

    footer form validation

--- manage-landing-page-master/main.js
+++ manage-landing-page-master/main.js
@@ -1,5 +1,5 @@
+// Mobile nav
 const burgerButton = document.getElementById('burger');
-
 const mobileNav = document.getElementById('mobile-nav');
 
 burgerButton.onclick = () => {
@@ -11,3 +11,30 @@ mobileNav.onclick = () => {
   mobileNav.classList.toggle('show');
   burgerButton.classList.toggle('active');
 }
+
+// Email validation
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Form validation
+const subscribeForm = document.getElementById('subscribe-form');
+const emailInput = document.querySelector('input[name="email"]');
+
+subscribeForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!validateEmail(emailInput.value)) {
+    emailInput.classList.add('error');
+    emailInput.nextElementSibling.classList.remove('hide');
+  } else {
+    emailInput.value = '';
+  }
+});
+
+emailInput.addEventListener('focus', () => {
+  emailInput.classList.remove('error');
+  emailInput.nextElementSibling.classList.add('hide');
+});
+
+
