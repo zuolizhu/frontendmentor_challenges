2077d768736101797aedde104d617d74b89506aa
zuolizhu <zuolizhu5960x@gmail.com>
2020-07-30 18:14:38 -0400

    basic setup for myteam project

---
+++ premium-myteam-master/myteam-sapper/src/routes/_layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<header>
+	<Nav {segment}/>
+</header>
+
+<main>
+	<slot></slot>
+</main>
+
+
+<style lang='scss' global>
+	@import './style/global.scss';
+</style>
---
+++ premium-myteam-master/myteam-sapper/src/routes/contact.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<h1>Contact this team</h1>
+
+<p>This is the 'contact' page. There's not much here.</p>
