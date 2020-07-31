81e2a153533f1c654fe63d94206be47683f955b9
zuolizhu <zuolizhu5960x@gmail.com>
2020-07-30 21:40:31 -0400

    header structure setup, mobile nav update

--- premium-myteam-master/myteam-sapper/src/components/Nav.svelte
+++ premium-myteam-master/myteam-sapper/src/components/Nav.svelte
@@ -1,57 +1,144 @@
 <script>
+	import { fly, fade } from 'svelte/transition';
+
+	// won't use segment in this project
 	export let segment;
+	
+	let burgerActive = false;
+	function handleBurger() {
+		burgerActive = !burgerActive;
+	}
 </script>
 
-<style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
+<style lang="scss">
+	.site-logo {
+		height: 3.2rem;
+		&__container {
+			width: 12.8rem;
+			height: 3.2rem;
+			display: inline-flex;
+			align-items: center;
+			img { width: 100%; }
+		}
 	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
+	.main-nav {
+		list-style-type: none;
+		display: inline-flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		padding: 4.8rem 2.4rem 0;
 		position: relative;
-		display: inline-block;
+		@media (min-width: 768px) {
+			padding: 6.4rem 4rem 0 3.9rem;
+		}
+	}
+	.desktop-nav {
+		display: none;
+		@media (min-width: 520px) {
+			display: inline-flex;
+			align-items: center;
+			justify-content: space-between;
+			width: 100%;
+			margin-left: 4.8rem;
+		}
+		@media (min-width: 560px) {
+			justify-content: flex-start;
+		}
+
+		&__item {
+			@media (min-width: 560px) {
+				&:nth-child(2) {
+					margin-left: 4rem;
+				}
+				&:nth-child(3) {
+					margin-left: auto;
+				}
+			}
+		}
+	}
+	.button--burger {
+		@media (max-width: 519px) {
+			display: inline-flex;
+			position: relative;
+			z-index: 10;
+		}
+		@media (min-width: 520px) {
+			display: none;
+		}
 	}
 
-	[aria-current]::after {
+	.mobile-nav-block {
+		@media (max-width: 519px) {
+			display: block;
+			position: absolute;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			height: 100vh;
+			background-color: rgba(0, 0, 0, .5);
+		}
+		@media (min-width: 520px) {
+			display: none;
+		}
+	}
+
+	.mobile-nav {
+		display: block;
 		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
+		background-color: var(--color-plice-blue);
+		background-image: url(../images/bg-pattern-about-1-mobile-nav-1.svg);
+		background-repeat: no-repeat;
+		background-position: right -10rem bottom;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		width: 68%;
+		padding-top: 11.2rem;
+		padding-left: 4.8rem;
 
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
+		&__item:nth-child(1) {
+			margin-bottom: 2.4rem;
+		}
+		&__item:nth-child(2) {
+			margin-bottom: 3.6rem;
+		}
 	}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-		<li><a aria-current="{segment === 'contact' ? 'page' : undefined}" href="contact">contact</a></li>
+<nav class="main-nav">
+	<!-- logo -->
+	<a class="site-logo" href=".">
+		<span class="sr-only">site logo</span>
+		<div class="site-logo__container">
+			<img src="./images/logo.svg" alt="site logo">
+		</div>
+	</a>
+	<!-- tablet/desktop menu -->
+	<ul class="desktop-nav">
+		<li class="desktop-nav__item"><a class="t-main-nav t-white" aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+		<li class="desktop-nav__item"><a class="t-main-nav t-white" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+		<li class="desktop-nav__item"><a class="t-main-nav t-white button button--contact" aria-current="{segment === 'contact' ? 'page' : undefined}" href="contact">contact</a></li>
 	</ul>
+	<!-- mobile burger -->
+	<button
+		on:click={handleBurger}
+		class="button button--burger">
+		{#if burgerActive}
+			<svg xmlns="http://www.w3.org/2000/svg" width="18" height="17"><path fill="#FFF" fill-rule="evenodd" d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"/></svg>
+		{:else}
+			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="17"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h20v3H0zM0 7h20v3H0zM0 14h20v3H0z"/></g></svg>
+		{/if}
+	</button>
+	<!-- mobile menu -->
+	{#if burgerActive}
+		<div transition:fade class="mobile-nav-block">
+			<ul transition:fly="{{x: 200}}" class="mobile-nav">
+				<li class="mobile-nav__item"><a class="t-main-nav t-white" aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+				<li class="mobile-nav__item"><a class="t-main-nav t-white" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+				<li class="mobile-nav__item"><a class="t-main-nav t-white button button--contact" aria-current="{segment === 'contact' ? 'page' : undefined}" href="contact">contact</a></li>
+			</ul>
+		</div>
+	{/if}
 </nav>
