d41baaba14aab814eb6bdf18e2e0e4b1632c6756
zuolizhu <zuolizhu5960x@gmail.com>
2020-08-07 01:57:38 -0400

    data messed up

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ const app = new Vue({
       {
         id: 1,
         projectName: 'Single price grid component',
+        techStacks: ['HTML', 'CSS'],
         previewImageLink: './single-price-grid-component-master/design/desktop-preview.jpg',
         solutionLink: 'https://www.frontendmentor.io/solutions/single-price-grid-component-solution-pure-html-css-utility-based-VAMyGbr-4',
         liveViewLink: './single-price-grid-component-master/index.html'
       },
       {
         id: 2,
+        techStacks: ['HTML', 'CSS'],
         projectName: 'Four cards feature section',
         previewImageLink: './four-card-feature-section-master/design/desktop-preview.jpg',
-        solutionLink: 'https://www.frontendmentor.io/solutions/single-price-grid-component-solution-pure-html-css-utility-based-VAMyGbr-4',
+        solutionLink: 'https://www.frontendmentor.io/solutions/four-card-feature-section-solution-pure-html-css-with-grid-DlDJHRCOM',
         liveViewLink: './four-card-feature-section-master/index.html'
       },
       {
         id: 3,
+        techStacks: ['HTML', 'CSS', 'JS'],
         projectName: 'Base apparel coming soon',
         previewImageLink: './base-apparel-coming-soon-master/design/desktop-preview.jpg',
-        solutionLink: 'https://www.frontendmentor.io/solutions/single-price-grid-component-solution-pure-html-css-utility-based-VAMyGbr-4',
+        solutionLink: 'https://www.frontendmentor.io/solutions/base-apparel-coming-soon-solution-html-css-with-js-form-validation-3-1ir2lMf',
         liveViewLink: './base-apparel-coming-soon-master/index.html'
       },
       {
@@ -34,22 +37,22 @@ const app = new Vue({
         id: 5,
         projectName: 'Intro component with signup form',
         previewImageLink: './intro-component-with-signup-form-master/design/desktop-preview.jpg',
-        solutionLink: 'https://www.frontendmentor.io/solutions/single-price-grid-component-solution-pure-html-css-utility-based-VAMyGbr-4',
+        solutionLink: 'https://www.frontendmentor.io/solutions/intro-component-with-signup-form-solution-with-form-validation-cnrjJBoxg',
         liveViewLink: './intro-component-with-signup-form-master/index.html'
       },
       {
         id: 6,
         projectName: 'Ping coming soon page',
         previewImageLink: './ping-coming-soon-page-master/design/desktop-preview.jpg',
-        solutionLink: 'https://www.frontendmentor.io/solutions/single-price-grid-component-solution-pure-html-css-utility-based-VAMyGbr-4',
+        solutionLink: 'https://www.frontendmentor.io/solutions/ping-single-column-coming-soon-solution-pure-html-css-v2HabpTrY',
         liveViewLink: './ping-coming-soon-page-master/index.html'
       },
       {
         id: 7,
-        projectName: 'Ping coming soon page',
-        previewImageLink: './ping-coming-soon-page-master/design/desktop-preview.jpg',
-        solutionLink: 'https://www.frontendmentor.io/solutions/single-price-grid-component-solution-pure-html-css-utility-based-VAMyGbr-4',
-        liveViewLink: './ping-coming-soon-page-master/index.html'
+        projectName: 'Huddle landing page with alternating feature blocks',
+        previewImageLink: './huddle-landing-page-with-alternating-feature-blocks-master/design/desktop-preview.jpg',
+        solutionLink: 'https://www.frontendmentor.io/solutions/huddle-landing-page-solution-pure-html-css-xSv7GfKN6',
+        liveViewLink: './huddle-landing-page-with-alternating-feature-blocks-master/index.html'
       },
       {
         id: 8,
