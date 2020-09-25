4f18ae9b3e84e888df4b89d47e3868f0d6b287c4
zuolizhu <zuolizhu5960x@gmail.com>
2020-09-25 15:13:41 -0400

    project init, gsap intro animation added

---
+++ room-homepage-master/js/app.js
@@ -0,0 +1,12 @@
+const timeline = gsap.timeline({
+  defaults:{ ease: 'power1.out' }
+});
+
+// Page loading slider
+timeline.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
+timeline.to('.intro-slider', { y: '-100%', duration: 1.5, delay: 0.5 });
+timeline.to('.intro', { y: '-100%', duration: 1 }, "-=1");
+
+// Sections animation
+timeline.fromTo('section.hero', {opacity: 0}, {opacity: 1, duration: 1});
+timeline.fromTo('section.about', {opacity: 0}, {opacity: 1, duration: 1}, "-=0.75");
---
+++ room-homepage-master/style-guide.md
@@ -0,0 +1,28 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Dark Gray: hsl(0, 0%, 63%)
+- Black: hsl(0, 0%, 0%)
+- White: hsl(0, 0%, 100%)
+- Very Dark Gray: hsl(0, 0%, 27%)
+
+## Typography
+
+### Body Copy
+
+- Font size: 12px
+
+### Font
+
+- Family: [Spartan](https://fonts.google.com/specimen/Spartan)
+- Weights: 500, 600, 700
