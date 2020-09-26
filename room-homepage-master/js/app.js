8fb7bf4652c8ca3d857d5dc3b5cd0f67c002513b
zuolizhu <zuolizhu5960x@gmail.com>
2020-09-26 02:23:22 -0400

    polished header

--- room-homepage-master/js/app.js
+++ room-homepage-master/js/app.js
@@ -10,8 +10,9 @@ timeline.to('.intro', { y: '-100%', duration: 1 }, "-=1");
 // Sections animation
 timeline.fromTo('section.hero', {opacity: 0}, {opacity: 1, duration: 1});
 timeline.fromTo('section.about', {opacity: 0}, {opacity: 1, duration: 1}, "-=0.75");
+timeline.fromTo('.header', {opacity: 0}, {opacity: 1, duration: 1});
 
-const mySiema = new Siema({ loop: true });
+const mySiema = new Siema({ loop: true, duration: 800 });
 const prevButtons = document.querySelectorAll('.siema-button-prev');
 const nextButtons = document.querySelectorAll('.siema-button-next');
 prevButtons.forEach(btn => {
@@ -21,3 +22,17 @@ nextButtons.forEach(btn => {
   btn.addEventListener('click', () => mySiema.next());
 });
 
+const burgerTrigger = document.querySelector('.mobile-nav-trigger');
+const navMobile = document.querySelector('.nav-mobile');
+const burgerIcons = document.querySelectorAll('.mobile-nav-trigger__icon');
+
+burgerTrigger.addEventListener('click', () => {
+  navMobile.classList.toggle('hide');
+  burgerIcons.forEach(icon => {
+    icon.classList.toggle('hide');
+  })
+});
+// autoplay
+// setTimeout(() => {
+//   setInterval(() => mySiema.next(), 5000);
+// }, 3000);
