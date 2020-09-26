1e71d62a0459c22bedb4b9a8c3107efdd3fa9cf6
zuolizhu <zuolizhu5960x@gmail.com>
2020-09-25 20:50:21 -0400

    basic layout finished

--- room-homepage-master/js/app.js
+++ room-homepage-master/js/app.js
@@ -10,3 +10,14 @@ timeline.to('.intro', { y: '-100%', duration: 1 }, "-=1");
 // Sections animation
 timeline.fromTo('section.hero', {opacity: 0}, {opacity: 1, duration: 1});
 timeline.fromTo('section.about', {opacity: 0}, {opacity: 1, duration: 1}, "-=0.75");
+
+const mySiema = new Siema({ loop: true });
+const prevButtons = document.querySelectorAll('.siema-button-prev');
+const nextButtons = document.querySelectorAll('.siema-button-next');
+prevButtons.forEach(btn => {
+  btn.addEventListener('click', () => mySiema.prev());
+});
+nextButtons.forEach(btn => {
+  btn.addEventListener('click', () => mySiema.next());
+});
+
